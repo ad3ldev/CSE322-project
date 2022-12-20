55c137d3f3dfa237d8037893e9d7e9b7697f2c0d
Gamal Abdul Hameed Nasef Newysar <es-gamalabdelhameed314@alexu.edu.eg>
2022-12-21 00:16:13 +0200

    Added checks on Appointment
    Check the existence of more than one appointment for the same user(Doctor / Patient) at the same time.

--- BackEnd/src/main/java/com/example/demo/Service/AppointmentService.java
+++ BackEnd/src/main/java/com/example/demo/Service/AppointmentService.java
@@ -1,11 +1,17 @@
 package com.example.demo.Service;
 
-import com.example.demo.Exception.BadRequestException;
 import com.example.demo.Models.Appointment;
+import com.example.demo.Models.Type;
 import com.example.demo.Repo.AppointmentRepository;
+import com.example.demo.Utils.Appointment_Result;
+import com.example.demo.Utils.State;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
+import org.json.JSONException;
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +19,7 @@ import java.util.List;
 @Service
 public class AppointmentService {
     private final AppointmentRepository appointmentRepository;
+
     public List<Appointment> getAllAppointments() {
         return appointmentRepository.findAll();
     }
@@ -34,19 +41,45 @@ public class AppointmentService {
         return patientAppointments;
     }
 
-    public void addAppointment(Appointment appointment) {
+    public Appointment_Result addAppointment(Appointment appointment) {
         boolean isAppointmentExist = appointmentRepository.existsById(appointment.getAppointmentPrimaryData());
         if (isAppointmentExist) {
-            throw new BadRequestException("Appointment is taken!");
+            return new Appointment_Result(State.FAILURE, "Appointment is taken!", null);
+        }
+        if (isUserBusy(appointment, Type.Doctor)) {
+            return new Appointment_Result(State.FAILURE, "The Doctor already has an appointment at this time!", null);
+        }
+        if (isUserBusy(appointment, Type.Patient)) {
+            return new Appointment_Result(State.FAILURE, "The Patient already has an appointment at this time!", null);
         }
         appointmentRepository.save(appointment);
+        return new Appointment_Result(State.SUCCESS, "Appointment successfully saved.", appointment);
     }
 
-    public void deleteAppointment(Appointment appointment) {
+    private boolean isUserBusy(Appointment appointment, Type type) {
+        Date date = appointment.getDate();
+        Time startTime = appointment.getStartTime();
+        Time endTime = appointment.getEndTime();
+        List<Appointment> appointments;
+        if (type == Type.Doctor)
+            appointments = getDoctorAppointments(appointment.getDoctorId());
+        else
+            appointments = getPatientAppointments(appointment.getPatientId());
+        for (Appointment app: appointments) {
+            if (app.getStartTime().equals(startTime)
+                    && app.getEndTime().equals(endTime)
+                    && app.getDate().equals(date))
+                return true;
+        }
+        return false;
+    }
+
+    public Appointment_Result deleteAppointment(Appointment appointment) {
         boolean isAppointmentExist = appointmentRepository.existsById(appointment.getAppointmentPrimaryData());
         if (!isAppointmentExist) {
-            throw new BadRequestException("Appointment is not Exist!");
+            return new Appointment_Result(State.FAILURE, "Appointment doesn't Exist!", null);
         }
         appointmentRepository.deleteById(appointment.getAppointmentPrimaryData());
+        return new Appointment_Result(State.SUCCESS, "Appointment successfully deleted.", appointment);
     }
 }
