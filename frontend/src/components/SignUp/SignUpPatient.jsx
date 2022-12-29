76ea451ead0fe680fcd10e037a058aae789b394a
Mo'men Gharib <momengharib22@hotmail.com>
2022-12-30 00:19:56 +0200

    User can attach his reports link.

--- frontend/src/components/SignUp/SignUpPatient.jsx
+++ frontend/src/components/SignUp/SignUpPatient.jsx
@@ -3,179 +3,187 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const SignUpPatient = () => {
-	const navigate = useNavigate();
-	const [patient, setPatient] = useState({
-		type: "Patient",
-		id: "",
-		gender: "",
-		age: "",
-		address: "",
-		name: "",
-		email: "",
-		password: "",
-		previousReports: "",
-	});
-	const handleInfo = (e) => {
-		const user = { ...patient };
-		if (e.target.id === "age") {
-			let x = parseInt(e.target.value, 10);
-			user[e.target.id] = x;
-		} else {
-			user[e.target.id] = e.target.value;
-		}
-		setPatient(user);
-	};
-	const createPatient = (e) => {
-		e.preventDefault();
+  const navigate = useNavigate();
+  const [patient, setPatient] = useState({
+    type: "Patient",
+    id: "",
+    gender: "",
+    age: "",
+    address: "",
+    name: "",
+    email: "",
+    password: "",
+    previousReports: "",
+  });
+  const handleInfo = (e) => {
+    const user = { ...patient };
+    if (e.target.id === "age") {
+      let x = parseInt(e.target.value, 10);
+      user[e.target.id] = x;
+    } else {
+      user[e.target.id] = e.target.value;
+    }
+    setPatient(user);
+    console.log(patient);
+  };
+  const createPatient = (e) => {
+    e.preventDefault();
 
-		if (
-			patient.email === "" ||
-			patient.password === "" ||
-			patient.name === ""
-		) {
-			showAlert();
-			setTimeout(removeAlert, 3000);
-			validateEmail(patient.email);
-		} else {
-			validateEmail(patient.email);
-			axios.post(`/signUp`, patient).then((response) => {
-				const res = response.data;
-				if (res.id >= 0 && res.state === "SUCCESS") {
-					navigate("/home", { state: res });
-				} else {
-					showAlert();
-				}
-			});
-		}
-	};
-	const emailAlert = document.querySelector(".email__alert");
+    if (
+      patient.email === "" ||
+      patient.password === "" ||
+      patient.name === ""
+    ) {
+      showAlert();
+      setTimeout(removeAlert, 3000);
+      validateEmail(patient.email);
+    } else {
+      validateEmail(patient.email);
+      axios.post(`/signUp`, patient).then((response) => {
+        const res = response.data;
+        if (res.id >= 0 && res.state === "SUCCESS") {
+          navigate("/home", { state: res });
+        } else {
+          showAlert();
+        }
+      });
+    }
+  };
+  const emailAlert = document.querySelector(".email__alert");
 
-	const validateEmail = (address) => {
-		const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (check.test(address) & (address !== null) & (address !== "")) {
-			check.test(address);
-		} else {
-			showAlert();
-			setTimeout(removeAlert, 3000);
-		}
-	};
+  const validateEmail = (address) => {
+    const check = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (check.test(address) & (address !== null) & (address !== "")) {
+      check.test(address);
+    } else {
+      showAlert();
+      setTimeout(removeAlert, 3000);
+    }
+  };
 
-	// email alerts
-	function showAlert() {
-		emailAlert.style.display = "block";
-	}
+  // email alerts
+  function showAlert() {
+    emailAlert.style.display = "block";
+  }
 
-	function removeAlert() {
-		emailAlert.style.display = "none";
-	}
-	return (
-		<div className='container'>
-			<form className='contact__form'>
-				<h2>Welcome!</h2>
-				<div className='Dname'>
-					<i className='far fa-envelope icon' />
-					<input
-						type='text'
-						placeholder='User Name'
-						id='name'
-						onChange={(e) => {
-							handleInfo(e);
-						}}
-					/>
-					<span className='field__name'>Name</span>
-					<small className='email__alert'>
-						Please enter a valid name
-					</small>
-				</div>
-				<div className='Demail'>
-					<i className='far fa-envelope icon' />
-					<input
-						type='text'
-						placeholder='Email'
-						id='email'
-						onChange={(e) => {
-							handleInfo(e);
-						}}
-					/>
-					<span className='field__name'>Email</span>
-					<small className='email__alert'>
-						Please enter a valid email address
-					</small>
-				</div>
+  function removeAlert() {
+    emailAlert.style.display = "none";
+  }
+  return (
+    <div className="container">
+      <form className="contact__form">
+        <h2>Welcome!</h2>
+        <div className="Dname">
+          <i className="far fa-envelope icon" />
+          <input
+            type="text"
+            placeholder="User Name"
+            id="name"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Name</span>
+          <small className="email__alert">Please enter a valid name</small>
+        </div>
+        <div className="Demail">
+          <i className="far fa-envelope icon" />
+          <input
+            type="text"
+            placeholder="Email"
+            id="email"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Email</span>
+          <small className="email__alert">
+            Please enter a valid email address
+          </small>
+        </div>
 
-				<div className='Demail'>
-					<i className='fas fa-lock icon' />
-					<input
-						type='password'
-						placeholder='Password'
-						id='password'
-						onChange={(e) => {
-							handleInfo(e);
-						}}
-					/>
-					<span className='field__name'>Password</span>
-					<small className='password__alert'>
-						6 to 20 characters and contain one number and one
-						uppercase letter
-					</small>
-				</div>
-				<div className='Demail'>
-					<i className='far fa-envelope icon' />
-					<input
-						type='text'
-						placeholder='Address'
-						id='address'
-						onChange={(e) => {
-							handleInfo(e);
-						}}
-					/>
-					<span className='field__name'>Address</span>
-					<small className='email__alert'>
-						Please enter a valid address
-					</small>
-				</div>
-				<div className='Dage'>
-					<i className='far fa-envelope icon' />
-					<input
-						type='text'
-						placeholder='Age'
-						id='age'
-						onChange={(e) => {
-							handleInfo(e);
-						}}
-					/>
-					<span className='field__name'>Age</span>
-					<small className='email__alert'>
-						Age can't be empty or negative
-					</small>
-				</div>
+        <div className="Demail">
+          <i className="fas fa-lock icon" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Password</span>
+          <small className="password__alert">
+            6 to 20 characters and contain one number and one uppercase letter
+          </small>
+        </div>
+        <div className="Demail">
+          <i className="far fa-envelope icon" />
+          <input
+            type="text"
+            placeholder="Address"
+            id="address"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Address</span>
+          <small className="email__alert">Please enter a valid address</small>
+        </div>
+        <div className="Demail">
+          <i className="far fa-envelope icon" />
+          <input
+            type="text"
+            placeholder="Put a link for previous Reports if any."
+            id="previousReports"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Report</span>
+        </div>
+        <div className="Dage">
+          <i className="far fa-envelope icon" />
+          <input
+            type="text"
+            placeholder="Age"
+            id="age"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          />
+          <span className="field__name">Age</span>
+          <small className="email__alert">Age can't be empty or negative</small>
+        </div>
 
-				<div className='Gender'>
-					<select
-						name='gender'
-						id='gender'
-						onChange={(e) => {
-							handleInfo(e);
-						}}>
-						<option value=''>Gender</option>
-						<option value='M'>Male</option>
-						<option value='F'>Female</option>
-					</select>
-				</div>
+        <div className="Gender">
+          <select
+            name="gender"
+            id="gender"
+            onChange={(e) => {
+              handleInfo(e);
+            }}
+          >
+            <option value="">Gender</option>
+            <option value="M">Male</option>
+            <option value="F">Female</option>
+          </select>
+        </div>
 
-				<div className='Pbottom'>
-					<button
-						type='submit'
-						className='submitBtn'
-						onClick={(e) => {
-							createPatient(e);
-						}}>
-						Sign Up
-					</button>
-				</div>
-			</form>
-		</div>
-	);
+        <div className="Pbottom">
+          <button
+            type="submit"
+            className="submitBtn"
+            onClick={(e) => {
+              createPatient(e);
+            }}
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPatient;
