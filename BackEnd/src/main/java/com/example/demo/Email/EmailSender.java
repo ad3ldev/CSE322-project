371fafd90cc7791f47c96372147302d37543f559
Mahmoud-Moh <69478720+Mahmoud-Moh@users.noreply.github.com>
2022-12-31 01:04:47 +0200

    Create EmailSender.java

---
+++ BackEnd/src/main/java/com/example/demo/Email/EmailSender.java
@@ -0,0 +1,32 @@
+package com.example.demo.Email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSender {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendEmail(String email, String patient_name, String doctor_name, String gender, String time) {
+        //System.out.println("sending email to" + email);
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom("rozlvet66@gmail.com");
+        msg.setTo(email);
+        String title = "";
+        if(gender.equals("M"))
+            title = "Mr.";
+        else
+            title = "Ms.";
+        msg.setSubject("Reminder");
+        msg.setText("Hello "+title+patient_name+".\nWe want to remind you " +
+                "that you have an appointment at "+time+". with Dr."+doctor_name+".\nStay Safe.");
+
+        javaMailSender.send(msg);
+    }
+
+}
