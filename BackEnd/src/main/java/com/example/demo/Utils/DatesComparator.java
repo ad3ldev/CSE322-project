11b70025456b6b83184af17ee9a863db07e0c9f1
Mahmoud-Moh <69478720+Mahmoud-Moh@users.noreply.github.com>
2022-12-31 08:53:30 +0200

    Update DatesComparator.java

--- BackEnd/src/main/java/com/example/demo/Utils/DatesComparator.java
+++ BackEnd/src/main/java/com/example/demo/Utils/DatesComparator.java
@@ -5,21 +5,12 @@ import java.util.Date;
 
 public class DatesComparator {
     public static boolean isToday(Date appointment_date, Date current_date){
-        boolean isToday = false, within_hour = false;
-        if(appointment_date.getDay() == current_date.getDay()
-        && appointment_date.getMonth() == current_date.getMonth()
-        && appointment_date.getYear() == current_date.getYear()){
-            isToday = true;
-        }
-
-        return isToday;
+        return appointment_date.getDay() == current_date.getDay()
+                && appointment_date.getMonth() == current_date.getMonth()
+                && appointment_date.getYear() == current_date.getYear();
     }
 
     public static boolean within_hour(Time startTime, Date current_date){
-        System.out.println("appointment time = "  + startTime.getHours() + "and current" + current_date.getHours());
-        if(startTime.getHours() - 1 == current_date.getHours())
-            return true;
-        else
-            return false;
+        return startTime.getHours() - 1 == current_date.getHours();
     }
 }
